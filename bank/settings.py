5d403e065b6485e17e2d55d05ff3626c322a88f3
Christian Luora <Christianluora47@gmail.com>
2025-11-01 06:24:14 +0300

    feat(auth): configure JWT authentication in settings.py

--- bank/settings.py
+++ bank/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'rest_framework',
+    'rest_framework_simplejwt'
     'accounts',
 
    
@@ -72,6 +75,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'bank.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+    'AUTH_HEADER_TYPES':('Bearer', ),
+
+}
 
 
 # Database
